8b3c6c3881b0f311db092094076e9535b7018e4f
Aleks-sv <aleks-sv@tut.by>
2022-10-21 17:14:30 +0300

    The fifth homework

---
+++ Task34/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int FindEvenNumbers(int[] array)
+{
+    int countNumbers = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) countNumbers = countNumbers + 1;
+    }
+    return countNumbers;
+}
+
+int[] arr = CreateArrayRndInt(5, 100, 999);
+PrintArray(arr);
+int findEvenNumbers = FindEvenNumbers(arr);
+Console.WriteLine($"Количество положительных трехзначных чисел = {findEvenNumbers}.");
---
+++ Task38/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и 
+//минимальным элементов массива.
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] array = new double[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        double rndNum = rnd.NextDouble() * (max - min) + min;
+        array[i] = Math.Round(rndNum, 2);
+    }
+    return array;
+}
+
+void PrintDoubleArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+double FindMinNumberArray(double[] array)
+{
+    double minNumber = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < minNumber) minNumber = array[i];
+    }
+    return minNumber;
+}
+
+double FindMaxNumberArray(double[] array)
+{
+    double maxNumber = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > maxNumber) maxNumber = array[i];
+    }
+    return maxNumber;
+}
+
+double[] arr = CreateArrayRndDouble(5, -10, 10);
+PrintDoubleArray(arr);
+double minNumber = FindMinNumberArray(arr);
+double maxNumber = FindMaxNumberArray(arr);
+
+if (minNumber < 0) Console.WriteLine($"Разница между максимальным и нимамальным значением = {Math.Round(maxNumber + minNumber, 2)}");
+else Console.WriteLine($"Разница между максимальным и нимамальным значением = {Math.Round(maxNumber - minNumber, 2)}");
