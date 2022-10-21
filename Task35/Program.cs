5c27736ae6cb62f95e3d0dbf6e13c90ad3bba82d
Aleks-sv <aleks-sv@tut.by>
2022-10-21 13:35:00 +0300

    The fifth seminar

---
+++ Task35/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых 
+// лежат в отрезке [10,99]
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int FindNumbers(int[] array)
+{
+    int sum = default;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (10 <= array[i] && array[i] <= 99) sum = sum + 1;
+    }
+    return sum;
+}
+
+int[] arr = CreateArrayRndInt(123, 0, 100);
+PrintArray(arr);
+
+Console.WriteLine(FindNumbers(arr));
---
+++ Task37/Program.cs
@@ -0,0 +1,45 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, 
+// второй и предпоследний и т.д. Результат запишите в новом массиве.
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] NewArray (int[] array)
+{
+    //int size = array.Length / 2;
+    //if (size % 2 == 1) size = size + 1;
+    int[] newArray = new int[array.Length / 2];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        newArray[i] = array[i] * array[array.Length - 1 - i];
+    }
+    //if (array.Length % 2 == 1) newArray.Length - 1 = array.Length - 1;
+    return newArray;
+
+}
+
+int[] arr = CreateArrayRndInt(5, 0, 10);
+PrintArray(arr);
+int[] newArray = NewArray(arr);
+PrintArray(newArray);
