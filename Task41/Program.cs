754f1153c201fe4436b3378bf93ad04e25ff2dec
Aleks-sv <aleks-sv@tut.by>
2022-10-27 13:51:14 +0300

    Homework Task41,43

---
+++ Task41/Program.cs
@@ -0,0 +1,41 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество чисел: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[count];
+for (int i = 0; i < count; i++)
+{
+    Console.Write($"{i + 1}-e число = ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int FindPositiveNumbers(int[] array)
+{
+    int numbers = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) numbers += 1;
+    }
+    return numbers;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int coun1 = FindPositiveNumbers(array);
+PrintArray(array);
+Console.Write($" -> {coun1}");
+
+
---
+++ Task43/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите первый числовой коэффициет для первой прямой: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите второй числовой коэффициет для первой прямой: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите первый числовой коэффициет для второй прямой: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите второй числовой коэффициет для второй прямой: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+FindPointIntersection(k1, b1, k2, b2);
+
+void FindPointIntersection (double pointOne, double pointTwo, double PointThree, double pointFour)
+{
+    double coordX = (pointFour - pointTwo)/(pointOne - PointThree);
+    double coordY = pointOne * coordX + pointTwo;
+    Console.WriteLine($"({Math.Round(coordX, 2)}, {Math.Round(coordY, 2)})");
+}
