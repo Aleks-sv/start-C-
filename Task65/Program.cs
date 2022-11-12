48adec167cb4f631f3e2fb605f09ccbe5e3723df
Aleksandr <aleks-sv@tut.by>
2022-11-12 21:25:31 +0300

    The last seminar

---
+++ Task65/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+Console.Write("Введите начальное число: ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите конечное число: ");
+int numberLast = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumber(int numFirst, int numLast)
+{
+    if (numFirst == numLast) Console.Write($"{numFirst}");
+    if (numFirst < numLast)
+    {
+        Console.Write($"{numFirst} ");
+        NaturalNumber(numFirst + 1, numLast);
+    }
+    if (numFirst > numLast)
+    {
+        Console.Write($"{numFirst} ");
+        NaturalNumber(numFirst - 1, numLast);
+    }
+    
+}
+NaturalNumber(numberFirst, numberLast);
---
+++ Task67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Write("Введите начальное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumElement (int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + SumElement(num / 10);
+
+}
+
+int sumElement = SumElement(number);
+Console.WriteLine($"{sumElement}");
